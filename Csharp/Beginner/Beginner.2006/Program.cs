056ca9ed51adedd7de7322936dbe54f45700cdc1
Vinicius <vini383@gmail.com>
2024-02-24 13:20:13 -0300

    feat: added new challenges

---
+++ Csharp/Beginner/Beginner.2006/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Beginner._2006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tipoCha = int.Parse(Console.ReadLine());
+            string[] stringparticipantes = Console.ReadLine().Split(' ');
+
+            List<int> intParticipantes = stringparticipantes.Select(int.Parse).ToList();
+
+            int numeroAcertos = 0;
+            
+            for (int i = 0; i < intParticipantes.Count; i++)
+            {
+                if (intParticipantes[i] == tipoCha)
+                {
+                    numeroAcertos++;
+                }
+            }
+            
+            Console.WriteLine(numeroAcertos);
+      
+
+        }
+    }
+}
