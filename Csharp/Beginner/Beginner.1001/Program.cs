7daeed7ee27af14b835f6991bcc1e3c4529403b9
Vinicius <vini383@gmail.com>
2024-02-24 12:00:39 -0300

    feat: added 1001

---
+++ Csharp/Beginner/Beginner.1001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Beginner._1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int result = A + B;
+            Console.WriteLine($"X = {result}");
+        }
+    }
+}
