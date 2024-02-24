c5cb6e688b558ead61a65bb34e2db41d9d8dee70
Vinicius <vini383@gmail.com>
2024-02-24 16:42:52 -0300

    added ad-hoc section

---
+++ Csharp/Ad-Hoc/Ad-Hoc/Ad-Hoc.1129/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+                break;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int[] values = Array.ConvertAll(input, int.Parse);
+
+                char result = ProcessQuestion(values);
+                Console.WriteLine(result);
+            }
+        }
+    }
+
+    static char ProcessQuestion(int[] values)
+    {
+        int blackCount = 0;
+        int whiteCount = 0;
+
+        for (int j = 0; j < values.Length; j++)
+        {
+            if (values[j] <= 127)
+                blackCount++;
+            else
+                whiteCount++;
+        }
+
+        if (blackCount == 1 && whiteCount == 4)
+        {
+            for (int j = 0; j < values.Length; j++)
+            {
+                if (values[j] <= 127)
+                    return GetOption(j);
+            }
+        }
+
+        return '*';
+    }
+
+    static char GetOption(int index)
+    {
+        return (char)('A' + index);
+    }
+}
