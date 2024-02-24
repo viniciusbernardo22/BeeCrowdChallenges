286c7a3d6f9d6bbb15da5faf7dc60b83e5570190
Vinicius <vini383@gmail.com>
2024-02-24 19:23:22 -0300

    feat: added new challenges

---
+++ Csharp/Beginner/Beginner.1038/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Beginner._1038
+{
+
+    class Lanche
+    {
+        public int Id { get; set; }
+        public decimal Preco { get; set; }
+        
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Lanche> lanches = new List<Lanche>
+            {
+                new Lanche { Id = 1, Preco = 4.00m },
+                new Lanche { Id = 2, Preco = 4.50m },
+                new Lanche { Id = 3, Preco = 5.00m },
+                new Lanche { Id = 4, Preco = 2.00m },
+                new Lanche { Id = 5, Preco = 1.50m },
+            };
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            int item = int.Parse(input[0]);
+            int quantidade = int.Parse(input[1]);
+            Lanche itemescolhido = lanches.Find(x => x.Id == item);
+
+            decimal total = quantidade * itemescolhido.Preco;
+            
+            Console.WriteLine($"Total: R$ {total}");
+        }
+    }
+}
---
+++ Csharp/Beginner/Beginner.1040/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beginner._1040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            CultureInfo culture = CultureInfo.InvariantCulture;
+            double nA = double.Parse(input[0], culture);
+            double nB = double.Parse(input[1], culture);
+            double nC = double.Parse(input[2], culture);
+            double nD = double.Parse(input[3], culture);
+            
+           
+            int pesoA = 2;
+            int pesoB = 3;
+            int pesoC = 4;
+            int pesoD = 1;
+            
+        
+            double media = (nA * pesoA + nB * pesoB + nC * pesoC + nD * pesoD) / (pesoA + pesoB + pesoC + pesoD);
+            Console.WriteLine($"Media: {media:F1}");
+            if (media > 7.0)
+            {
+                Console.WriteLine("Aluno aprovado.");
+                Environment.Exit(0);
+            }
+            else if(media < 5.0)
+            {
+                Console.WriteLine("Aluno reprovado.");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Aluno em exame.");
+                double NotaExame = double.Parse(Console.ReadLine(), new CultureInfo("en-US"));
+                Console.WriteLine($"Nota do exame: {NotaExame:F1}");
+                
+                double novaMedia = (NotaExame + media) / 2;
+                
+               
+                if (novaMedia >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                }
+                
+                Console.WriteLine($"Media final: {novaMedia:F1}");
+            }
+          
+        }
+    }
+}
