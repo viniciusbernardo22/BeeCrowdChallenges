fa3f7b4be72413ff63e14e526876e5bec08090dd
Vinicius <vini383@gmail.com>
2024-02-24 11:52:07 -0300

    structural change

---
+++ Csharp/Beginner/BeeCrowdBeginner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeeCrowdBeginner._1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
